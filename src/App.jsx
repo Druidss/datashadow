0da3effba001783db79826173dc597b254a21e83
Druidss <druids8012@gmail.com>
2024-04-03 02:31:11 +0200

    add curlnosie animation

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { Canvas } from '@react-three/fiber';
 import './App.css';
 import Basic from './ParticlesDemo';
 import { OrbitControls } from "@react-three/drei";
+import Fbo from './fbo/FBOParticles.jsx'
 
 
 
@@ -10,8 +11,8 @@ function App() {
     <>
       <Canvas  camera={{ position: [1.5, 1.5, 1.5] }}>
         <ambientLight intensity={0.5} />
-        <color attach='background' args={['#101010']} />
-        <Basic  count={2000} shape="sphere" />
+        {/* <Basic  count={2000} shape="sphere" /> */}
+        <Fbo></Fbo>
          <OrbitControls autoRotate />
       </Canvas>
     </>
